443a114e5240eaa120db20ed002b278c49d1fbf8
M-sharkawy <mostafasharks021@gmail.com>
2025-05-22 19:49:15 +0300

    task 1,2,3,4,5,6

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Filtered logger module"""
+from typing import List
+import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """returns formatted log record"""
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.msg, self.SEPARATOR)
+        return (super(RedactingFormatter, self).format(record))
+
+
+Pii_Feilds = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    """returns a logging.Logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(Pii_Feilds)))
+    logger.addHandler(stream_handler)
+
+    return (logger)
+
+
+def filter_datum(fields: List[str],
+                 redaction: str, message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return (message)
