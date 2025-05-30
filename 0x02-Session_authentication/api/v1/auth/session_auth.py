bb3522b0a4f11c45de2f410b512b906dbff4b2eb
M-sharkawy <mostafasharks021@gmail.com>
2025-05-30 03:10:10 +0300

    task 0 1 2 3 4 5 6 7

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Session authentication module"""
+from api.v1.auth.auth import Auth
+import uuid
+from typing import Dict
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """Session authentication class"""
+    user_id_by_session_id: Dict[str, str] = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns a User ID based on a Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return (None)
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return (None)
+
+        return (User.get(user_id))
